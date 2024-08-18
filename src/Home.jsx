bda586437205938a56f0048fa4f19aaa98d0a29a
MuhammadXYZ <muhammadusmanali070@gmail.com>
2024-08-18 22:14:58 +0500

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import working from './assets/404.png'
+export default function Home() {
+  return (
+   <>
+   <h3 className='WOrkingTxt'>Sorry we are working on this site</h3>
+
+        <img className='working' src={working} alt="" />
+   </>
+  )
+}
